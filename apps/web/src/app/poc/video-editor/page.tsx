4a9bc29e92e324a6472fb20769a32a71e101ecea
Benno <57860196+bennoinbeta@users.noreply.github.com>
2024-10-25 14:32:59 +0200

    #15 moved video editor into pock path

--- apps/web/src/app/poc/video-editor/page.tsx
+++ apps/web/src/app/poc/video-editor/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { VideoEditor } from '@/components';
 
 const Page: React.FC = () => {
-	return <main>Hello World</main>;
+	return <VideoEditor />;
 };
 
 export default Page;
